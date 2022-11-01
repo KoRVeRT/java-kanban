dfcf6da8652b36700bd9e1bfc1ae16c21ea1732d
Dmitry Masharov <staker@yandex.ru>
2022-11-01 19:21:29 +0300

    feat: изменить реализацию List для переменной viewHistory класс                 InMemoryHistoryManager на LinkedList.       добавить константу COUNT_RECORDS с возможным количеством записей в истории = 10.       упростить метод add() на добавление подзадачи, чтобы условие удаляло 1 запись в начале списке, при количестве записей = 10 штук.

--- src/ru/yandex/practicum/tasktracker/service/InMemoryHistoryManager.java
+++ src/ru/yandex/practicum/tasktracker/service/InMemoryHistoryManager.java
@@ -2,20 +2,19 @@ package ru.yandex.practicum.tasktracker.service;
 
 import ru.yandex.practicum.tasktracker.model.Task;
 
-import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 public class InMemoryHistoryManager implements HistoryManager {
-    private final List<Task> viewHistory = new ArrayList<>();
+    private final List<Task> viewHistory = new LinkedList<>();
+    private final int COUNT_RECORDS = 10;
 
     @Override
-    public  void add(Task task) {
-        if (viewHistory.size() < 10) {
-            viewHistory.add(task);
-        } else {
+    public void add(Task task) {
+        if (viewHistory.size() == COUNT_RECORDS) {
             viewHistory.remove(0);
-            viewHistory.add(task);
         }
+        viewHistory.add(task);
     }
 
     @Override
